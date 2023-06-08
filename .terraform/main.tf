d1c6e4d60b77d2292053d17e091aaf1999b6adf5
modestemax <modestemax@gmail.com>
2023-06-08 16:20:33 +0100

    test

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -13,6 +13,26 @@ terraform {
     bucket = var.bucket_name
 #    acl = "private"
   }
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+#  routing_rule {
+#    condition {
+#      key_prefix_equals = "docs/"
+#    }
+#    redirect {
+#      replace_key_prefix_with = "documents/"
+#    }
+#  }
+}
 #resource "aws_s3_bucket_ownership_controls" "example" {
 #  bucket = aws_s3_bucket.bucket.id
 #  rule {
@@ -52,7 +72,9 @@ locals {
         }
       }
     }
-#    distribution_config {
+    default_root_object = "index.html"
+
+    #    distribution_config {
 #      price_class = "PriceClass_All"
 #      comment = "My awesome CloudFront"
 #      enabled = true
