f735f77a775e1c36af7ee7988b88d920e42b6448
walter0b <jakinjoram5@outlook.com>
2023-06-25 18:23:14 +0100

    Fix bug

--- components/ProfileDropdown.tsx
+++ components/ProfileDropdown.tsx
@@ -1,49 +1,49 @@
-import { useState } from 'react';
-import { useAppDispatch } from '../store';
+// import { useState } from 'react';
+// import { useAppDispatch } from '../store';
 
-const Profile: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const dispatch = useAppDispatch();
+// const Profile: React.FC = () => {
+//     const [isOpen, setIsOpen] = useState(false);
+//     const dispatch = useAppDispatch();
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+//     const toggleDropdown = () => {
+//         setIsOpen(!isOpen);
+//     };
 
-    const handleClickOutside = (event: MouseEvent) => {
-        const target = event.target as HTMLElement;
-        if (!target.closest('.profile-dropdown')) {
-            setIsOpen(false);
-        }
-    };
+//     const handleClickOutside = (event: MouseEvent) => {
+//         const target = event.target as HTMLElement;
+//         if (!target.closest('.profile-dropdown')) {
+//             setIsOpen(false);
+//         }
+//     };
 
-    // Listen for clicks outside the dropdown to close it
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
+//     // Listen for clicks outside the dropdown to close it
+//     useEffect(() => {
+//         document.addEventListener('mousedown', handleClickOutside);
+//         return () => {
+//             document.removeEventListener('mousedown', handleClickOutside);
+//         };
+//     }, []);
 
-    const handleLogout = () => {
-        // Dispatch your logout action here
-        dispatch(logout());
-    };
+//     const handleLogout = () => {
+//         // Dispatch your logout action here
+//         dispatch(logout());
+//     };
 
-    return (
-        <div className="profile-dropdown">
-            <img
-                src="/path/to/profile-picture.png"
-                alt="Profile Picture"
-                onClick={toggleDropdown}
-            />
-            {isOpen && (
-                <ul className="dropdown-menu">
-                    <li>My Account</li>
-                    <li onClick={handleLogout}>Logout</li>
-                </ul>
-            )}
-        </div>
-    );
-};
+//     return (
+//         <div className="profile-dropdown">
+//             <img
+//                 src="/path/to/profile-picture.png"
+//                 alt="Profile Picture"
+//                 onClick={toggleDropdown}
+//             />
+//             {isOpen && (
+//                 <ul className="dropdown-menu">
+//                     <li>My Account</li>
+//                     <li onClick={handleLogout}>Logout</li>
+//                 </ul>
+//             )}
+//         </div>
+//     );
+// };
 
-export default Profile;
+// export default Profile;
