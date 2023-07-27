db745aa37bd544f3833192fea6321c00d5cd87f7
walter0b <jakinjoram5@outlook.com>
2023-07-27 13:33:02 +0100

    remove list pagination

--- src/components/atom/pagination.tsx
+++ src/components/atom/pagination.tsx
@@ -75,35 +75,32 @@ const Pagination: React.FC<PaginationProps> = ({
     };
 
     return (
-        <div>
-            <ul className="inline-flex -space-x-px mt-3 flex-wrap">
-                <li>
-                    <button
-                        onClick={() => gotoPage(currentPage - 1)}
-                        disabled={!canPreviousPage}
-                        className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#111827] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >
-                        <span className="sr-only">Previous</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512">
-                            <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
-                        </svg>
-                    </button>
-                </li>
-                {renderPageButtons()}
-                <li>
-                    <button
-                        onClick={() => gotoPage(currentPage + 1)}
-                        disabled={!canNextPage}
-                        className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#111827] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >
-                        <span className="sr-only">Next</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512">
-                            <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
-                        </svg>
-                    </button>
-                </li>
-            </ul>
-        </div>
+      <div className="flex justify-center mt-3">
+  <button
+    onClick={() => gotoPage(currentPage - 1)}
+    disabled={!canPreviousPage}
+    className="pagination-button"
+  >
+    <span className="sr-only">Previous</span>
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512">
+      <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
+    </svg>
+  </button>
+
+  {renderPageButtons()}
+
+  <button
+    onClick={() => gotoPage(currentPage + 1)}
+    disabled={!canNextPage}
+    className="pagination-button"
+  >
+    <span className="sr-only">Next</span>
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512">
+      <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
+    </svg>
+  </button>
+</div>
+
     );
 };
 
