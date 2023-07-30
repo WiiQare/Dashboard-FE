d1537f7755e85209a6397b8fce7249dc33dce7a9
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2023-07-30 16:57:34 -0400

    test: beneficiaries page

---
+++ test/pages/Beneficiaries/index.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import Beneficiaries from "../../../src/pages/Beneficiaries";
+
+jest.mock("next/router", () => ({
+  replace: jest.fn(),
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    replace: jest.fn(),
+  }),
+}));
+
+test("renders Providers component", async () => {
+  const { container } = render(<Beneficiaries />);
+  expect(container).toMatchSnapshot();
+});
